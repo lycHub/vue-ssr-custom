4f0850d33ddad20bd1b8e4033d400a6c8ed1642c
lycHub <1120805364@qq.com>
2021-02-15 11:23:49 +0800

    css 分离

--- configs/client/prod.js
+++ configs/client/prod.js
@@ -2,6 +2,7 @@ const { merge } = require('webpack-merge')
 const { join } = require('path')
 const baseConfig = require('../base.js')
 const VueClientPlugin = require('vue-server-renderer/client-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
@@ -9,7 +10,19 @@ module.exports = merge(baseConfig, {
   output: {
     path: join(__dirname, '../../dist/client')
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  },
   plugins: [
-    new VueClientPlugin()
+    new VueClientPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:8].css',
+      chunkFilename: '[id].[contenthash:8].css'
+    })
   ]
 });
